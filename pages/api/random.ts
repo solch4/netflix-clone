ff2fdcd7dcd4d40b2dbdc1283e94585801f3811f
solch4 <sol.maldonado.0209@gmail.com>
2023-05-10 00:34:24 -0300

    random movie endpoint added

---
+++ pages/api/random.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function hanlder(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req);
+
+    const movieCount = await prismadb.movie.count();
+    const randomIndex = Math.floor(Math.random() * movieCount);
+
+    const [randomMovie] = await prismadb.movie.findMany({
+      take: 1,
+      skip: randomIndex,
+    });
+
+    res.status(200).json(randomMovie);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
