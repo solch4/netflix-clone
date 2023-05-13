41369ae8cd943328143e5f8c12d1d098833bfd70
solch4 <sol.maldonado.0209@gmail.com>
2023-05-12 21:28:20 -0300

    movies endpoint

---
+++ pages/api/movies/index.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import serverAuth from "@/lib/serverAuth";
+import prismadb from "@/lib/prismadb";
+
+export default async function hanlder(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req);
+    const movies = await prismadb.movie.findMany();
+    res.status(200).json(movies);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
