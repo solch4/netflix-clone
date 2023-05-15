4573ef879b1cb90f5786aaf2d4eb8f247df31d52
solch4 <sol.maldonado.0209@gmail.com>
2023-05-15 20:57:21 -0300

    favorite & favorites endpoint added

---
+++ pages/api/favorite.ts
@@ -0,0 +1,68 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { without } from "lodash";
+import serverAuth from "@/lib/serverAuth";
+import prismadb from "@/lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method === "POST") {
+      const { currentUser } = await serverAuth(req);
+      const { movieId } = req.body;
+
+      const existingMovie = await prismadb.movie.findUnique({
+        where: {
+          id: movieId,
+        },
+      });
+
+      if (!existingMovie) throw new Error("Invalid ID");
+
+      const user = prismadb.user.update({
+        where: {
+          email: currentUser.email || undefined,
+        },
+        data: {
+          favoriteIds: {
+            push: movieId,
+          },
+        },
+      });
+
+      res.status(200).json(user);
+    }
+
+    if (req.method === "DELETE") {
+      const { currentUser } = await serverAuth(req);
+      const { movieId } = req.body;
+
+      const existingMovie = await prismadb.movie.findUnique({
+        where: {
+          id: movieId,
+        },
+      });
+
+      if (!existingMovie) throw new Error("Invalid ID");
+
+      const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+
+      const updatedUser = prismadb.user.update({
+        where: {
+          email: currentUser.email || undefined,
+        },
+        data: {
+          favoriteIds: updatedFavoriteIds,
+        },
+      });
+
+      res.status(200).json(updatedUser);
+    }
+
+    return res.status(405).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
